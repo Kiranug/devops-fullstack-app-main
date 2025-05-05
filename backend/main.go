baef3e83cf11949f526a4f05f244c12f2a9d73c7
Kiranug <kiran.ug123@gmail.com>
2025-05-05 10:18:49 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -4,53 +4,62 @@ import (
 	"employees/controller"
 	"employees/repository"
 	"employees/routes"
-	"employees/service"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
-	"os"
 )
 
 func main() {
-
 	app := fiber.New()
-	app.Use(cors.New())
+
+	// Enable CORS
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: os.Getenv("ALLOWED_ORIGINS"),
+		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
+
+	// Initialize database
 	db := initializeDatabaseConnection()
 	repository.RunMigrations(db)
+
+	// Setup dependencies
 	employeeRepository := repository.NewEmployeeRepository(db)
 	employeeService := service.NewEmployeeService(employeeRepository)
 	employeeController := controller.NewEmployeeController(employeeService)
+
+	// Register routes
 	routes.RegisterRoute(app, employeeController)
+
+	// Start the server
 	err := app.Listen(":8080")
 	if err != nil {
 		log.Fatalln(fmt.Sprintf("error starting the server %s", err.Error()))
 	}
 }
+
 func initializeDatabaseConnection() *gorm.DB {
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  createDsn(),
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
+
 	if err != nil {
 		log.Fatalln(fmt.Sprintf("error connecting with database %s", err.Error()))
 	}
-
 	return db
 }
 
 func createDsn() string {
+	// Hardcoded values for testing
+	dbHost := "172.174.2.135"
+	dbUser := "myuser"
+	dbPassword := "mypassword"
+	dbName := "CREATEDB"
+	dbPort := "5432"
+
 	dsnFormat := "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable"
-	dbHost := os.Getenv("172.174.2.135")
-	dbUser := os.Getenv("myuser")
-	dbPassword := os.Getenv("mypassword")
-	dbName := os.Getenv("CREATEDB")
-	dbPort := os.Getenv("5432")
 	return fmt.Sprintf(dsnFormat, dbHost, dbUser, dbPassword, dbName, dbPort)
 }
